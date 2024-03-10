d1023c8f4af1045beaff9bfe5f25dfbb0150eeb9
Mota <joaopedromota67@gmail.com>
2024-03-10 16:57:32 -0300

    10/03/2024 16:57

--- Portifolio/menu.js
+++ Portifolio/menu.js
@@ -3,3 +3,20 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 250)
 })
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('mobile-menu')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
